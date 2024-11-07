0970eea664124baadb35162e5bd9baed44a28288
Pramikaofficial <pramikaofficial@gmail.com>
2024-11-07 15:51:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello world <code>Pramika</code>22cb041 .
         </p>
         <a
           className="App-link"
